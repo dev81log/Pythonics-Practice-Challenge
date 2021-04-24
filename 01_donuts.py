e85bba7908ab5baed9e50675d9d14fb2c2fd3d04
Lipzeng <berdinazzijf@gmail.com>
2021-04-24 12:46:47 -0300

    final_exercicio

--- 01_donuts.py
+++ 01_donuts.py
@@ -10,12 +10,14 @@ e donuts(23) retorna 'Number of donuts: many'
 """
 
 def donuts(count):
-    # +++ SUA SOLUÇÃO +++
-    if count < 10:
-        msg = f'Number of donuts: {count}'
-    else:
-        msg = 'Number of donuts: many'
-    return msg
+    return 'Number of donuts: {}'.format(count if count < 10 else 'many')
+    
+    # # +++ SUA SOLUÇÃO +++
+    # if count < 10:
+    #     msg = f'Number of donuts: {count}'
+    # else:
+    #     msg = 'Number of donuts: many'
+    # return msg
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
 
